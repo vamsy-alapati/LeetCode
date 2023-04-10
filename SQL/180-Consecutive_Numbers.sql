df3f22c26b61d836a9df8568886135ef8646a5aa
VAMSY ALAPATI <alapati.vamsy1996@gmail.com>
2023-04-10 11:56:19 -0400

    added 180-Consecutive_Numbers.sql

---
+++ SQL/180-Consecutive_Numbers.sql
@@ -0,0 +1,10 @@
+-- MySQL
+
+-- MEDIUM
+
+select distinct(l1.num) ConsecutiveNums
+from logs l1, logs l2, logs l3
+where l1.num = l2.num
+and l2.num = l3.num
+and l2.id = l1.id + 1
+and l3.id = l2.id + 1;
