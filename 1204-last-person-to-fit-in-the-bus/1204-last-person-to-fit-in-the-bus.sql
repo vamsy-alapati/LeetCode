060fab57d1f8d71a47220bbddc68e1b0e1cc9831
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2023-04-16 18:57:23 -0400

    Time: 4149 ms (5.11%), Space: 0B (100.00%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -3,7 +3,7 @@
 select q1.person_name
       from queue q1, queue q2
      where q2.turn <= q1.turn
-     group by q1.person_name
+     group by q1.turn
      having sum(q2.weight) <= 1000
      order by sum(q2.weight) desc
      limit 1;
