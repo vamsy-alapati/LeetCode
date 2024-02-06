622add09a030b6f64e38f8c7a8d6e9b3a5784a19
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2024-02-06 11:29:58 -0500

    Time: 4 ms (65.93%), Space: 44.7 MB (53.50%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,37 @@
 class Solution {
+    
+    // BRUTE FORCE METHOD
+//     public int[] twoSum(int[] nums, int target) {
+        
+//         for (int i=0; i<nums.length; i++) {
+//             for (int j=i+1; j<nums.length; j++) {
+//                 if (nums[i]+nums[j] == target) {
+//                     return new int[] {i,j};
+//                 }
+//             }
+//         }
+//         return null;
+        
+//     }
+    
+    
+    // Two-Pass Hash Table
     public int[] twoSum(int[] nums, int target) {
         
+        //Create a HashMap and insert all elements of array into the map 
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i<nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        
+        //for each element of array check if it's complement exists in map and return it's index along with array element index
         for (int i=0; i<nums.length; i++) {
-            for (int j=i+1; j<nums.length; j++) {
-                if (nums[i]+nums[j] == target) {
-                    return new int[] {i,j};
-                }
+            int complement = target - nums[i];
+            if(map.containsKey(complement) && map.get(complement) != i) {
+                return new int[] {i, map.get(complement)};
             }
         }
+        
         return null;
         
     }
