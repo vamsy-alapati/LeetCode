be83f995d6c842b4909d4372e1a58c7b1466e703
VAMSY ALAPATI <alapati.vamsy1996@gmail.com>
2024-02-18 23:51:13 -0500

    updated

--- Algorithms-Java/0231-power-of-two/0231-power-of-two.java
+++ Algorithms-Java/0231-power-of-two/0231-power-of-two.java
@@ -1,5 +1,6 @@
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        
         return (n>0) && (n & (n-1)) == 0;  
         // bit wise operation any 2 power and minus 1 is always 0
         // example 4 -> 100 but 3 -> 011
