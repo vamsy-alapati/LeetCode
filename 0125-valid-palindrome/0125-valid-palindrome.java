02ed73893c4f81333b508cc031cde362b2f96d8e
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2024-02-18 23:43:06 -0500

    Time: 15 ms (29.29%), Space: 44.9 MB (32.82%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,8 +1,10 @@
 class Solution {
     public boolean isPalindrome(String s) {
         
+        // Convert to lowercase and remove all non-alphanumeric characters
         s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
         
+        // placeholder to c= hold the reverse str in StringBuilder
         StringBuilder str = new StringBuilder();
         str.append(s);
         
