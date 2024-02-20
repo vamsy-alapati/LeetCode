287776d9b16b3f43e0958a322b5914f2fed8e00b
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2024-02-20 14:14:58 -0500

    Time: 3 ms (63.41%), Space: 53.1 MB (7.18%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,6 +1,8 @@
 class Solution {
     public int majorityElement(int[] nums) {
         
+        // if an element occurs more than n/2 times, after sorting the array, 
+        // the element at index n/2 should be that same
         int n = nums.length;
         
         Arrays.sort(nums);
