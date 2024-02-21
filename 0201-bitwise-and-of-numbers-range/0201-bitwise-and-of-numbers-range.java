c1ab04b36684f60560cac3cd32cecb9792db4de3
Vamsy Alapati <52825189+vamsy-alapati@users.noreply.github.com>
2024-02-20 19:09:12 -0500

    Time: 3 ms (100.00%), Space: 43.9 MB (54.81%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -1,6 +1,7 @@
 class Solution {
     public int rangeBitwiseAnd(int left, int right) {
         
+        // trick is in to start from large number and move to small
         while(right>left)
            right = right & right-1;
 
